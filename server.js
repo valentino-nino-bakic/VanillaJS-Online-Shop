0830557335b886a0f3728a2afb04969b7ebaa83e
Nino <web52995@gmail.com>
2024-05-12 18:19:27 +0200

    connected with MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = process.env.PORT;
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+
 
 app.use(express.json());
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(DB_CONNECTION_STRING);
+        console.log('Successfully connected with our database!');
+    } catch (error) {
+        console.log(error);
+    }
+}
+connectToDatabase();
+
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about_us');
+});
+
 
 
 

5ea8cf263fd615f1dbc9d8aea7b17d97d98e18b5
Nino <web52995@gmail.com>
2024-05-20 06:23:17 +0200

    added products to database

---
+++ routes/productRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const ProductController = require('../controllers/productController');
+
+
+
+router.get('/products/:id', ProductController.getProduct);
+router.get('/products', ProductController.getProducts);
+router.post('/products', ProductController.addProduct);
+
+
+module.exports = router;

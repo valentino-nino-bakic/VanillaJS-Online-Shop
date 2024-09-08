b36d2b1e9bf86eb87f21351204b531d95705712b
applegrammer <web52995@gmail.com>
2024-09-08 05:44:33 +0200

    display orders page

--- routes/adminViewRoutes.js
+++ routes/adminViewRoutes.js
@@ -14,25 +14,33 @@ router.get('/', async (req, res) => {
     res.render('admin', { username: 'YORKSHIRE', section: 'users', users: users });
 });
 
+
 router.get('/users', async (req, res) => {
     const users = await User.find();
     res.render('admin', { username: 'YORKSHIRE', section: 'users', users: users });
 });
+
 
 router.get('/products', async (req, res) => {
     const products = await Product.find();
     res.render('admin', { username: 'YORKSHIRE', section: 'products', products: products });
 });
 
+
 router.get('/orders', async (req, res) => {
-    const orders = await Cart.find();
+    const orders = await Cart.find()
+        .populate('userId')
+        .populate('products.productId');
+
     res.render('admin', { username: 'YORKSHIRE', section: 'orders', orders: orders });
 });
+
 
 router.get('/messages', async (req, res) => {
     const messages = await ContactMessage.find();
     res.render('admin', { username: 'YORKSHIRE', section: 'messages', messages: messages });
 });
+
 
 router.get('/settings', (req, res) => {
     res.render('admin', { username: 'YORKSHIRE', section: 'settings' });
